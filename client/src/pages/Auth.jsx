2be5b0a8bd7bc3897e3dfe82c16ce59e24a9fba2
Vologdin Semen <vologdin.dev@gmail.com>
2022-08-29 23:39:48 +0500

    Init client side

---
+++ client/src/pages/Auth.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Auth = () => {
+    return (
+        <div>
+            <h5>AUTH</h5>
+        </div>
+    );
+};
+
+export default Auth;
---
+++ client/src/pages/DevicePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DevicePage = () => {
+    return (
+        <div>
+            <h2>DEVICE PAGE</h2>
+        </div>
+    );
+};
+
+export default DevicePage;
